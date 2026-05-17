d6dce3385cc56ff92253767e7847aadc860d9a79
Rabiul-Sujon <alamrabiul889@gmail.com>
2026-05-18 03:19:01 +0600

    feature: added User, Room and Booking mongoose models

---
+++ models/Room.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const roomSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    floor: {
+        type: String,
+        required: true
+    },
+    capacity: {
+        type: Number,
+        required: true
+    },
+    hourlyRate: {
+        type: Number,
+        required: true
+    },
+    amenities: [
+        {
+            type: String
+        }
+    ],
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    ownerEmail: {
+        type: String,
+        required: true
+    },
+    bookingCount: {
+        type: Number,
+        default: 0
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Room', roomSchema);
